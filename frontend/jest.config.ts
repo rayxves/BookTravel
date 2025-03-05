4966444c04140298696882aeef3f5c483acd6bd3
Rayssa Guimarães <guimaraesr305@gmail.com>
2025-03-05 18:21:32 -0300

    config jest

--- frontend/jest.config.ts
+++ frontend/jest.config.ts
@@ -1,6 +1,12 @@
-module.exports = {
-    preset: 'ts-jest',
-    testEnvironment: 'jsdom',
-    setupFilesAfterEnv: ['@testing-library/jest-dom/extend-expect'],
-  };
-  
+import type { Config } from 'jest';
+import dotenv from 'dotenv';
+
+dotenv.config({ path: '.env.local' });
+
+const config: Config = {
+  preset: 'ts-jest',
+  testEnvironment: 'jest-environment-jsdom',
+  setupFilesAfterEnv: ['@testing-library/jest-dom', 'jest-canvas-mock'],
+};
+
+export default config;
