2369594e6a8a3780252f544f9e471aaea9efa102
Rayssa Guimarães <guimaraesr305@gmail.com>
2025-04-03 08:22:15 -0300

    update data to match with api response

--- frontend/app/data/popularTouristSpots.ts
+++ frontend/app/data/popularTouristSpots.ts
@@ -1,38 +1,50 @@
 export const popularTouristSpots = [
   {
     name: "Eiffel Tower",
-    photoUrls: [
-      `https://maps.googleapis.com/maps/api/place/photo?maxwidth=400&photoreference=AUy1YQ2N6M9v2chHLaSNdjWOSqox9V7RYr2Yy9mHLEBqupGTOgXi35Hf1ax6jXD0LsRXL7MrG4doiIglvrnMwfKzpqcVCRs3ELTDTr7PyfhEGfs06Q-P2r22Ldq6eXHQTA_OP_k8Wq6yTFHz1Ksh3djpK2jPc8ugAUpiR6R232DCmwl4q7Ouo-t_XbYMGgKwrtOgbm24wbyiYD8kmgPWhhvRji2yv8bMy9iljl-h61kxswxVcBuWnCJMFxj_1LgjGFgHLq9NKBhhP9Ku0nraddt95S2IBHlkh2-bcplfvCQwLzm8XR1PBNffJoLQax_CK3REh7r8bwmPQ-Q&key=${process.env.NEXT_PUBLIC_GOOGLE_PLACES_API_KEY}`,
-    ],
+    photos: [
+      {
+        photoReference: "AUy1YQ2N6M9v2chHLaSNdjWOSqox9V7RYr2Yy9mHLEBqupGTOgXi35Hf1ax6jXD0LsRXL7MrG4doiIglvrnMwfKzpqcVCRs3ELTDTr7PyfhEGfs06Q-P2r22Ldq6eXHQTA_OP_k8Wq6yTFHz1Ksh3djpK2jPc8ugAUpiR6R232DCmwl4q7Ouo-t_XbYMGgKwrtOgbm24wbyiYD8kmgPWhhvRji2yv8bMy9iljl-h61kxswxVcBuWnCJMFxj_1LgjGFgHLq9NKBhhP9Ku0nraddt95S2IBHlkh2-bcplfvCQwLzm8XR1PBNffJoLQax_CK3REh7r8bwmPQ-Q"
+      }
+    ]
   },
   {
     name: "Great Wall of China",
-    photoUrls: [
-      `https://maps.googleapis.com/maps/api/place/photo?maxwidth=400&photoreference=AUy1YQ0PGtx9VLRXh1cMscPlyxUafTyR3H-FejjCE9GuupOEPRtinrKwGDHodu4FQTvrwKdGJdXMS6xhXwJXC2YKOTI02Oc3H7dk7_bhnSVQD2J0mfddNWsgphBveNogE2XLaP0XhOL-28ndOyXSihaVyZVv4z_gLpCj2SnLEy9LJDd0aNYvvk_vYjJPIbztbxfHVIM6QjhjJ8P1uK0vzGHPW48bpu2MjDhcODuCgQWrfLUINyo1anMKu8kBf0NgrdA--USla-6TRe8MUoAlSW_ffg4twYcPbj9W2fJ6ycZf_dsynnyV5csnQm7mJqV8Ejf83iF7L96R_qo&key=${process.env.NEXT_PUBLIC_GOOGLE_PLACES_API_KEY}`,
-    ],
+    photos: [
+      {
+        photoReference: "AUy1YQ0PGtx9VLRXh1cMscPlyxUafTyR3H-FejjCE9GuupOEPRtinrKwGDHodu4FQTvrwKdGJdXMS6xhXwJXC2YKOTI02Oc3H7dk7_bhnSVQD2J0mfddNWsgphBveNogE2XLaP0XhOL-28ndOyXSihaVyZVv4z_gLpCj2SnLEy9LJDd0aNYvvk_vYjJPIbztbxfHVIM6QjhjJ8P1uK0vzGHPW48bpu2MjDhcODuCgQWrfLUINyo1anMKu8kBf0NgrdA--USla-6TRe8MUoAlSW_ffg4twYcPbj9W2fJ6ycZf_dsynnyV5csnQm7mJqV8Ejf83iF7L96R_qo"
+      }
+    ]
   },
   {
     name: "Colosseum",
-    photoUrls: [
-      `https://maps.googleapis.com/maps/api/place/photo?maxwidth=400&photoreference=AUy1YQ1DJdA4LpuZa5ErCvXUeNJNwdSRDMiwQJ1UR3oGz2LdxicstLbhMSs5o6tuHCVLjQ2DQ3Fg3QMVYpRxC_OYoxvBjnPc_CuVKAABCJHzWjIJp4h4pxNS7ZCBirNk1uW3OPtaVOxMcM7skZg6nt91H5cO6pXzQdbc4_-eiPdErAMIZXptjQu13z8pNTmwaiATFsfgz0B2dv7Dp6NuNabrtHKWYpZNTbPPAZQ3-MiOfX8xZ0JHV8RY8IdvclJ8KNC6c8tqp9f-ZbAnWKY5gKD7ScDRV67DNR-OVOj2Mm77JYE8nvQ6oKkoAtZMhHvTIua1G2v1sLjTqLU&key=${process.env.NEXT_PUBLIC_GOOGLE_PLACES_API_KEY}`,
-    ],
+    photos: [
+      {
+        photoReference: "AUy1YQ1DJdA4LpuZa5ErCvXUeNJNwdSRDMiwQJ1UR3oGz2LdxicstLbhMSs5o6tuHCVLjQ2DQ3Fg3QMVYpRxC_OYoxvBjnPc_CuVKAABCJHzWjIJp4h4pxNS7ZCBirNk1uW3OPtaVOxMcM7skZg6nt91H5cO6pXzQdbc4_-eiPdErAMIZXptjQu13z8pNTmwaiATFsfgz0B2dv7Dp6NuNabrtHKWYpZNTbPPAZQ3-MiOfX8xZ0JHV8RY8IdvclJ8KNC6c8tqp9f-ZbAnWKY5gKD7ScDRV67DNR-OVOj2Mm77JYE8nvQ6oKkoAtZMhHvTIua1G2v1sLjTqLU"
+      }
+    ]
   },
   {
     name: "Great Barrier Reef",
-    photoUrls: [
-      `https://maps.googleapis.com/maps/api/place/photo?maxwidth=400&photoreference=AUy1YQ1GyF1fP9boPye3bFpRYu1APfNb4xmE-DZ-zMW8mR5c90imIpWYdph20yjA26OfNWL_ozZYbVDmoelc7tCUvoZWZRV-Y-nMZJpy0iMzrwM_LAIJ4DBSmQPMG7_B2QGk0NOVXMgJMHFZmUzy5-sMP4gJlQugdh8tzYVLecYenW8lj8PFwIv3ndGlv8C6bnQdWe3f7jgXnTld4M5k8PWR_NjWFzcOkMgN7PmulqdwxjBLPb8rV-x5zkscTx8qI8UJlrUjAKjj5I2HfnvRDDJN7s2MP1D4yGFjXoJlO7EhUpGG0F-mRo9jVwDpchDHp4QE6WLdOyDRPBQ&key=${process.env.NEXT_PUBLIC_GOOGLE_PLACES_API_KEY}`,
-    ],
+    photos: [
+      {
+        photoReference: "AUy1YQ1GyF1fP9boPye3bFpRYu1APfNb4xmE-DZ-zMW8mR5c90imIpWYdph20yjA26OfNWL_ozZYbVDmoelc7tCUvoZWZRV-Y-nMZJpy0iMzrwM_LAIJ4DBSmQPMG7_B2QGk0NOVXMgJMHFZmUzy5-sMP4gJlQugdh8tzYVLecYenW8lj8PFwIv3ndGlv8C6bnQdWe3f7jgXnTld4M5k8PWR_NjWFzcOkMgN7PmulqdwxjBLPb8rV-x5zkscTx8qI8UJlrUjAKjj5I2HfnvRDDJN7s2MP1D4yGFjXoJlO7EhUpGG0F-mRo9jVwDpchDHp4QE6WLdOyDRPBQ"
+      }
+    ]
   },
   {
     name: "Taj Mahal",
-    photoUrls: [
-      `https://maps.googleapis.com/maps/api/place/photo?maxwidth=400&photoreference=AUy1YQ2q3VQN0lfB_ZZJsY-hkxxSlVdIbkx-I_qRL44Unr7Zhxlt6HOtYxx6Q40D7k5yQ7MPgF7tshNf_56738096FfhvjASg1y2ROU4-Y4R3fJG_DIdt6uxL1YpcAg0mNll2KSFZbC1lTndzWqfx6ytS9iVmXoybTsXzL-Js91TxZ7RxYbTdsKO-SJRBGeyvL6J77guvqbBRmvNRp48Ot6qUNkmvKGY0r_eiedq01JUGSLxAj5jIpKmVpbgVKScDX8ntC3Df9gc0y3ys1WKvZQO03Npa-ZL5lxwQba1zRjsigDLilzFQ1WcnlCooyvZa8C3i1Y3-Cupg_D98ZYiPPWI-NbY5xirs98LV84bY3umKM2ZcaRgN-laCGSNJMsloDmAza19DH9nRG7XPodOC7iq0Rml9N9j7zc6JgWkC0HpqsJq9JBBwRbUwqn0XXXOmB0Dp1wHp68l4oqK1B2My1KAT47GdKM0PN5YwcFMVzrNA5H9dd4je_VdEgrO9KkiD_nXC2-BPBS-4gzqsw3oMOtfhEo8ZrDf4wnizLp7e4CEm1gSAHAYRU4L7Oqi9eJASqEt1mndRjApugtQP2wByEqij3nOy1dXWEik3kEwQ2NEfSMUa97q&key=${process.env.NEXT_PUBLIC_GOOGLE_PLACES_API_KEY}`,
-    ],
+    photos: [
+      {
+        photoReference: "AUy1YQ2q3VQN0lfB_ZZJsY-hkxxSlVdIbkx-I_qRL44Unr7Zhxlt6HOtYxx6Q40D7k5yQ7MPgF7tshNf_56738096FfhvjASg1y2ROU4-Y4R3fJG_DIdt6uxL1YpcAg0mNll2KSFZbC1lTndzWqfx6ytS9iVmXoybTsXzL-Js91TxZ7RxYbTdsKO-SJRBGeyvL6J77guvqbBRmvNRp48Ot6qUNkmvKGY0r_eiedq01JUGSLxAj5jIpKmVpbgVKScDX8ntC3Df9gc0y3ys1WKvZQO03Npa-ZL5lxwQba1zRjsigDLilzFQ1WcnlCooyvZa8C3i1Y3-Cupg_D98ZYiPPWI-NbY5xirs98LV84bY3umKM2ZcaRgN-laCGSNJMsloDmAza19DH9nRG7XPodOC7iq0Rml9N9j7zc6JgWkC0HpqsJq9JBBwRbUwqn0XXXOmB0Dp1wHp68l4oqK1B2My1KAT47GdKM0PN5YwcFMVzrNA5H9dd4je_VdEgrO9KkiD_nXC2-BPBS-4gzqsw3oMOtfhEo8ZrDf4wnizLp7e4CEm1gSAHAYRU4L7Oqi9eJASqEt1mndRjApugtQP2wByEqij3nOy1dXWEik3kEwQ2NEfSMUa97q"
+      }
+    ]
   },
   {
     name: "Machu Picchu",
-    photoUrls: [
-      `https://maps.googleapis.com/maps/api/place/photo?maxwidth=400&photoreference=AUy1YQ0aYqqwBIM0_cshBCIpwzqLUHvoqw_BJK9bWW_P7sINFPSovQtl9XnU9qVsJaxF_HR3R3iSc93I9xvtdEzGL8WUSeBVQh9pf2aCl6IpKFVGh8m0DUANabSCJpFsxVH0shXMYc2sDBoEdpIxXmDf2fmTE6gshJlWHSY3xoSVdMsLzVgJLp9wZ0Bx67Ec4xHx5XVmTZ4X3DO-YvvWGj3p_QCLu-BUH7X06zNsmOT1Pmxn10hq3ZiyQUlRL5Y3NIqxwkyRW6_iEm3uTXEenNd_I4SD9IzKy7Fkee0yy2ZjDRwtPpK3CzgXz0vcAaeo5Nj3E3uCXwCJutU&key=${process.env.NEXT_PUBLIC_GOOGLE_PLACES_API_KEY}`,
-    ],
-  },
+    photos: [
+      {
+        photoReference: "AUy1YQ0aYqqwBIM0_cshBCIpwzqLUHvoqw_BJK9bWW_P7sINFPSovQtl9XnU9qVsJaxF_HR3R3iSc93I9xvtdEzGL8WUSeBVQh9pf2aCl6IpKFVGh8m0DUANabSCJpFsxVH0shXMYc2sDBoEdpIxXmDf2fmTE6gshJlWHSY3xoSVdMsLzVgJLp9wZ0Bx67Ec4xHx5XVmTZ4X3DO-YvvWGj3p_QCLu-BUH7X06zNsmOT1Pmxn10hq3ZiyQUlRL5Y3NIqxwkyRW6_iEm3uTXEenNd_I4SD9IzKy7Fkee0yy2ZjDRwtPpK3CzgXz0vcAaeo5Nj3E3uCXwCJutU"
+      }
+    ]
+  }
 ];
--- frontend/app/data/travelHighlights.ts
+++ frontend/app/data/travelHighlights.ts
@@ -4,8 +4,10 @@ export const travelHighlights = [
     name: "China",
     description: "China",
     rating: 0,
-    photoUrls: [
-      `https://maps.googleapis.com/maps/api/place/photo?maxwidth=400&photoreference=AUy1YQ1IbUeAOZ5eXbg8BwdAFXrRc0YcELR-i0liS3O-hKoQ3xAQe8AjoHUMVEcpDRnYAOUvo8XLgPzhXXPrhB3bvSVjk4-qKjJN073mp1uAibZhpdbo_01Ylu5ValNnQFqzVk9NOkyGV7xvvh77OLbl3OZ3zvx0dfktQNMuDsJIWEa-Kd2usL3QbgugmAZuv5zaY9Sd4D-4s4aMRfFo2H7eRPXvE7fQkAyotmnlqLHuo970Y9V4FCyI_eN-5C2rMdIhn9L7Brh-X-kLdpl1iRDK9OBZzFbZkkAvv0VWPfFTqmaZhNSaoVQZMnm2chqob6HBbJSKIYPDW6p4zsFSecg-CTlZsWxSuwcc_EFiKaDlpNorCmQurFPnKWTI4Q7E7lj2P-Bevv5OwKEUpI4FsbwQ2ebH6VS9cZHEcbptlU0iOKwOzQ-I&key=${process.env.NEXT_PUBLIC_GOOGLE_PLACES_API_KEY}`
+    photos: [
+      {
+        photoReference: "AUy1YQ1IbUeAOZ5eXbg8BwdAFXrRc0YcELR-i0liS3O-hKoQ3xAQe8AjoHUMVEcpDRnYAOUvo8XLgPzhXXPrhB3bvSVjk4-qKjJN073mp1uAibZhpdbo_01Ylu5ValNnQFqzVk9NOkyGV7xvvh77OLbl3OZ3zvx0dfktQNMuDsJIWEa-Kd2usL3QbgugmAZuv5zaY9Sd4D-4s4aMRfFo2H7eRPXvE7fQkAyotmnlqLHuo970Y9V4FCyI_eN-5C2rMdIhn9L7Brh-X-kLdpl1iRDK9OBZzFbZkkAvv0VWPfFTqmaZhNSaoVQZMnm2chqob6HBbJSKIYPDW6p4zsFSecg-CTlZsWxSuwcc_EFiKaDlpNorCmQurFPnKWTI4Q7E7lj2P-Bevv5OwKEUpI4FsbwQ2ebH6VS9cZHEcbptlU0iOKwOzQ-I"
+      }
     ]
   },
   {
@@ -13,8 +15,10 @@ export const travelHighlights = [
     name: "Paris",
     description: "Paris, France",
     rating: 0,
-    photoUrls: [
-      `https://maps.googleapis.com/maps/api/place/photo?maxwidth=400&photoreference=AUy1YQ2eO8slWB1VpTRZsdQ1Z28a8GR4XQhNlFKLZfI3MMzB0HMYBeUXnCYvg0C5DYU_4i4mrvD3JN2cmbrCeeS_cwV6BSKUKLwqZAekzFTwj0ANkJiAkeDMJty-V11m-ZqFBj2TU2OMiwpt-kBUjkZtArQBedfq3SAdsKbarFsx8gNuWnkhige0RRMaecYhARoylgoWqemCMIUACjpQgrI7FalbnOEwIcsuKE7mNnte6Xx6FQ8Xe8_VUglREMl9OmbU-nqBZht95fGRyTqQJkBBEtnnalmxpTQkwwObsOEBl7kXAXMKqp_Le3pEi6ecd49fE8EYXg_iRY_gb6hrmelOSTwn13aI43J1TJjqBQBfLbpIpxY-ocdl_XNI0X1MNB5ZbsTD4xVmArYI0xf9TicF2uPGmVHt75s89_56gmEGf5U&key=${process.env.NEXT_PUBLIC_GOOGLE_PLACES_API_KEY}`
+    photos: [
+      {
+        photoReference: "AUy1YQ2eO8slWB1VpTRZsdQ1Z28a8GR4XQhNlFKLZfI3MMzB0HMYBeUXnCYvg0C5DYU_4i4mrvD3JN2cmbrCeeS_cwV6BSKUKLwqZAekzFTwj0ANkJiAkeDMJty-V11m-ZqFBj2TU2OMiwpt-kBUjkZtArQBedfq3SAdsKbarFsx8gNuWnkhige0RRMaecYhARoylgoWqemCMIUACjpQgrI7FalbnOEwIcsuKE7mNnte6Xx6FQ8Xe8_VUglREMl9OmbU-nqBZht95fGRyTqQJkBBEtnnalmxpTQkwwObsOEBl7kXAXMKqp_Le3pEi6ecd49fE8EYXg_iRY_gb6hrmelOSTwn13aI43J1TJjqBQBfLbpIpxY-ocdl_XNI0X1MNB5ZbsTD4xVmArYI0xf9TicF2uPGmVHt75s89_56gmEGf5U"
+      }
     ]
   },
   {
@@ -22,8 +26,10 @@ export const travelHighlights = [
     name: "Russia",
     description: "Russia",
     rating: 0,
-    photoUrls: [
-      `https://maps.googleapis.com/maps/api/place/photo?maxwidth=400&photoreference=AUy1YQ0Cz5ViuTBJ1TYIPbeJzQzE_ha2YyikzdoreOcT52XAlMM6m0unxUKmk8GJkphZbxKLs1QfjUNOd5bzEujxvhiefJQqhszuiaXsiOhMc6xOvMnpACgvtQnaYAsZlGYdwUEAvstxsvGy8Ckit2iaT_4Rc3jf-5Hc7ArDDILb8anfOUn2k6DEm07wg2M1zoUoKw1DXhsloq3ZP1EQup1swB8fP61vHXoyrczE5NonqCAW3KSmw_9q5-Uw3wGPAOE5RBm_xSJ2-RWcb_ewPwOwRJ6tcc5q4hcK8KLI9Z76tY3Rgm25cYS1k1-uaR22IbrXnMiQGhGhK7vTRZFkirrprJtkrxsy-1LHXMszHJwRvXuJVGwxjMbSr9gPDrYpbBaQey4WOlI5tOZvILrhh1ScndV4ZoUztMmQvm3joztSXp8O&key=${process.env.NEXT_PUBLIC_GOOGLE_PLACES_API_KEY}`
+    photos: [
+      {
+        photoReference: "AUy1YQ0Cz5ViuTBJ1TYIPbeJzQzE_ha2YyikzdoreOcT52XAlMM6m0unxUKmk8GJkphZbxKLs1QfjUNOd5bzEujxvhiefJQqhszuiaXsiOhMc6xOvMnpACgvtQnaYAsZlGYdwUEAvstxsvGy8Ckit2iaT_4Rc3jf-5Hc7ArDDILb8anfOUn2k6DEm07wg2M1zoUoKw1DXhsloq3ZP1EQup1swB8fP61vHXoyrczE5NonqCAW3KSmw_9q5-Uw3wGPAOE5RBm_xSJ2-RWcb_ewPwOwRJ6tcc5q4hcK8KLI9Z76tY3Rgm25cYS1k1-uaR22IbrXnMiQGhGhK7vTRZFkirrprJtkrxsy-1LHXMszHJwRvXuJVGwxjMbSr9gPDrYpbBaQey4WOlI5tOZvILrhh1ScndV4ZoUztMmQvm3joztSXp8O"
+      }
     ]
   },
   {
@@ -31,8 +37,10 @@ export const travelHighlights = [
     name: "Maldives",
     description: "Maldives",
     rating: 0,
-    photoUrls: [
-      `https://maps.googleapis.com/maps/api/place/photo?maxwidth=400&photoreference=AUy1YQ3FEwpCmCDpV0CpDnXM2RawQpQurLmMwJSInsaO2q6O_WH1XsAtloXJmWeW05WtbARp92VmyfnoRnvJANezImZU2IjERfBWIQ8ix5HntYQk5mA-sZ0SzfYomcILBwVfyYJu1gb-hoJsDLdliCoy0drkbbE1oe8xCmTjl1UA9qqyrz4su0DEF9wGxfZZTLmZX9JgU_KSxNceFu12j1CmScaMDUJS0KRyE54PJWvsv5F9rjYcUYCrz7bAzfor1uBUFPQNCU7nK_tcpykDoC1j4ohdkGhKyy0bfdfUQxmkS8crtbrYEbcWJkcdkTM18-GbNqwRF-oSWxw7VSknYJyzLAUcjSZw8YE9aP9nw2jRQ4ExptqhW2bO64fCec2-WGrjlBiXmCkCKHOCberkPnWpp-MfN6ep5m76NcqhDj95bpzTvw&key=${process.env.NEXT_PUBLIC_GOOGLE_PLACES_API_KEY}`
+    photos: [
+      {
+        photoReference: "AUy1YQ3FEwpCmCDpV0CpDnXM2RawQpQurLmMwJSInsaO2q6O_WH1XsAtloXJmWeW05WtbARp92VmyfnoRnvJANezImZU2IjERfBWIQ8ix5HntYQk5mA-sZ0SzfYomcILBwVfyYJu1gb-hoJsDLdliCoy0drkbbE1oe8xCmTjl1UA9qqyrz4su0DEF9wGxfZZTLmZX9JgU_KSxNceFu12j1CmScaMDUJS0KRyE54PJWvsv5F9rjYcUYCrz7bAzfor1uBUFPQNCU7nK_tcpykDoC1j4ohdkGhKyy0bfdfUQxmkS8crtbrYEbcWJkcdkTM18-GbNqwRF-oSWxw7VSknYJyzLAUcjSZw8YE9aP9nw2jRQ4ExptqhW2bO64fCec2-WGrjlBiXmCkCKHOCberkPnWpp-MfN6ep5m76NcqhDj95bpzTvw"
+      }
     ]
   },
   {
@@ -40,8 +48,10 @@ export const travelHighlights = [
     name: "Canada",
     description: "Canada",
     rating: 0,
-    photoUrls: [
-      `https://maps.googleapis.com/maps/api/place/photo?maxwidth=400&photoreference=AUy1YQ27rw7M27baTuWCptUfADdxwI8swz18F_0c9XbiPb-H2zfKNDbEOzIhTcnLhe-k2p7uECjXJTzSRX1yWeWMSm-yqx7JYml0h9Tfyu4ljFY_TGB2Cm3NPUse7c_TyhEkBTLmDcTeFN_GLrZ_kikbtB63STz_vYE8LkZv3muv82rIqakoeYkSosPn8bOFZabUSNZIxQzpB03md2B-FLqqLl9_twudyIWxQqeVAWdUpyLRPq_mHiXBU5EAVBb_5IN0k9vQmD_5QbQ-QzZdXUjHNoBw23acXbFtKde3B7HogllWlJ8RgsdzU_ARsEeZ9g9mT_RoL9Pwyycn-Pw31a82cc3oTytaS_qsYEDqW6qieeQLXbWC-RZiVm-C-Qd2USNFIJRut6X1Qhcukjx8XSmgeARyF8QPkbHcH-6xvvM9alCc&key=${process.env.NEXT_PUBLIC_GOOGLE_PLACES_API_KEY}`
+    photos: [
+      {
+        photoReference: "AUy1YQ27rw7M27baTuWCptUfADdxwI8swz18F_0c9XbiPb-H2zfKNDbEOzIhTcnLhe-k2p7uECjXJTzSRX1yWeWMSm-yqx7JYml0h9Tfyu4ljFY_TGB2Cm3NPUse7c_TyhEkBTLmDcTeFN_GLrZ_kikbtB63STz_vYE8LkZv3muv82rIqakoeYkSosPn8bOFZabUSNZIxQzpB03md2B-FLqqLl9_twudyIWxQqeVAWdUpyLRPq_mHiXBU5EAVBb_5IN0k9vQmD_5QbQ-QzZdXUjHNoBw23acXbFtKde3B7HogllWlJ8RgsdzU_ARsEeZ9g9mT_RoL9Pwyycn-Pw31a82cc3oTytaS_qsYEDqW6qieeQLXbWC-RZiVm-C-Qd2USNFIJRut6X1Qhcukjx8XSmgeARyF8QPkbHcH-6xvvM9alCc"
+      }
     ]
   },
   {
@@ -49,9 +59,10 @@ export const travelHighlights = [
     name: "Brazil",
     description: "Brazil",
     rating: 0,
-    photoUrls: [
-      `https://maps.googleapis.com/maps/api/place/photo?maxwidth=400&photoreference=AWYs27zJecDkrN-JXXgFVBce_OBeHeeP0Rl_k1s2t6EVrbdZzs-ZeO1fAwOC3PX5HetU6DNn1ZH6-ECoiJ7VomQ0fljKFeZN33kedYmjlkXUZq6eB3QzBxpiBF564zJH1sJPKMbgM-qUC1G96u1xe0Cju22z0mtJJ2mhn9Yud-Ot-isr5B8&key=${process.env.NEXT_PUBLIC_GOOGLE_PLACES_API_KEY}`,
-      `https://maps.googleapis.com/maps/api/place/photo?maxwidth=400&photoreference=AWYs27z3gK0kNybVYKaI_Xokpz7-rbqjUesL3h-NDP-_2_xSt-M-NMUFldLdFBcZBybW6T2Qq3fAJaJDla93ilQnt4WTN8HV9HTWllR3djegReo5Kebzg_5MsnK-3OUSfBBjJKy175fFD0crbPpIP6VBNmTIC4CVUnMlpQ9MB72BtYfN_sQ7&key=${process.env.NEXT_PUBLIC_GOOGLE_PLACES_API_KEY}`
+    photos: [
+      {
+        photoReference: "AWYs27zJecDkrN-JXXgFVBce_OBeHeeP0Rl_k1s2t6EVrbdZzs-ZeO1fAwOC3PX5HetU6DNn1ZH6-ECoiJ7VomQ0fljKFeZN33kedYmjlkXUZq6eB3QzBxpiBF564zJH1sJPKMbgM-qUC1G96u1xe0Cju22z0mtJJ2mhn9Yud-Ot-isr5B8"
+      }
     ]
   }
 ];
