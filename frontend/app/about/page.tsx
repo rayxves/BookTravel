5eaeee19dea2cea50b23ef51108fb3b72acfb71a
Rayssa Guimarães <guimaraesr305@gmail.com>
2025-04-14 15:03:02 -0300

    add about page

---
+++ frontend/app/about/page.tsx
@@ -0,0 +1,51 @@
+"use client";
+
+import menWalking from "@/public/menWalking.json";
+import Lottie from "lottie-react";
+import FeatureList from "../components/About/FeatureList";
+
+export default function About() {
+  return (
+    <>
+      {" "}
+      <div className="w-full min-h-screen overflow-y-auto pb-8 bg-gray-200 font-inter">
+        <div className="flex flex-col items-center min-h-screen p-6 bg-gray-200 relative">
+          <h1 className="text-center font-poppins text-gray-800 text-2xl md:text-3xl border-b border-gray-800 w-full pb-4 mb-5">
+            Welcome to Book Travel!
+          </h1>
+
+          <p className="text-gray-800 text-sm md:text-md lg:text-lg xl:text-xl leading-relaxed py-4 w-full">
+            Your platform to explore and discover your dream destinations.
+            Created as part of a personal project, this site is designed to help
+            you plan trips and organize your favorite spots with ease.
+          </p>
+
+          <h2 className="text-gray-800 text-xl md:text-2xl my-5 w-full">
+            How does it work?
+          </h2>
+
+          <FeatureList />
+
+          <div className="flex flex-col lg:flex-row justify-between items-center w-full pt-6 lg:pt-12">
+            <p className="text-gray-800 text-sm md:text-md lg:text-lg leading-relaxed w-full lg:w-2/3 pb-8">
+              This project was developed with the purpose of learning and
+              studying web development, exploring API creation, front-end
+              integration, and enhancing design and usability skills. The idea
+              was to build a functional platform where you can search, filter,
+              save, and organize travel destinations while practicing modern
+              technologies. Built with ♥.
+            </p>
+
+            <div className="w-full sm:w-1/2 lg:w-1/3 flex justify-center items-start">
+              <Lottie
+                animationData={menWalking}
+                loop
+                style={{ width: "50%", height: "50%" }}
+              />
+            </div>
+          </div>
+        </div>
+      </div>
+    </>
+  );
+}
---
+++ frontend/app/components/About/FeatureList.tsx
@@ -0,0 +1,79 @@
+"use client";
+import { useState } from "react";
+import { motion, AnimatePresence } from "framer-motion";
+
+const sections = [
+  {
+    title: "Search",
+    content:
+      "Search for your favorite destinations directly in our search bar to get detailed information about each location.",
+  },
+  {
+    title: "Filters",
+    content:
+      "Filter destinations by type, price, and rating to find exactly what you're looking for.",
+  },
+  {
+    title: "Nearby",
+    content:
+      "Use our 'next to me' feature to discover places close to your current location and filter results.",
+  },
+  {
+    title: "Account",
+    content:
+      "Log in to personalize your experience! Save the places you like most and access your favorite spots quickly.",
+  },
+  {
+    title: "Favorites",
+    content:
+      "When you find a place you'd like to visit, click 'add to favorites'. This way, you can easily return to it whenever you want.",
+  },
+];
+
+export default function FeatureList() {
+  const [openIndex, setOpenIndex] = useState<number | null>(null);
+
+  return (
+    <div className="w-full grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-6 px-4">
+      {sections.map((item, index) => {
+        const isOpen = openIndex === index;
+
+        return (
+          <div
+            key={index}
+            className="relative bg-white/10 rounded-xl shadow-md overflow-visible"
+          >
+            <button
+              onClick={() => setOpenIndex(isOpen ? null : index)}
+              className="w-full text-left px-6 py-4 font-semibold text-gray-800 text-md flex justify-between items-center cursor-pointer"
+            >
+              <span>{item.title}</span>
+              <motion.span
+                initial={{ rotate: 0 }}
+                animate={{ rotate: isOpen ? 180 : 0 }}
+                transition={{ duration: 0.3 }}
+              >
+                ▼
+              </motion.span>
+            </button>
+
+            <AnimatePresence>
+              {isOpen && (
+                <motion.div
+                  key="dropdown"
+                  initial={{ opacity: 0, scale: 0.95, y: -10 }}
+                  animate={{ opacity: 1, scale: 1, y: 0 }}
+                  exit={{ opacity: 0, scale: 0.95, y: -10 }}
+                  transition={{ duration: 0.2 }}
+                  className="absolute z-10 top-full left-0 mt-1 w-full bg-gray-100 text-gray-700 text-sm rounded-b-xl shadow-lg px-6 py-4"
+                >
+                  {item.content}
+                </motion.div>
+              )}
+            </AnimatePresence>
+          </div>
+        );
+      })}
+    </div>
+  );
+}
